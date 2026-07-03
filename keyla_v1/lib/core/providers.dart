9b392cfe34cd78834dcb43c181c154d82082e4c7
TheObsidianEye-ARIF-FOYSAL <ariffoysalruetcse21@gmail.com>
2026-07-04 02:39:06 +0600

    security services 11

--- keyla_v1/lib/core/providers.dart
+++ keyla_v1/lib/core/providers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:sodium_libs/sodium_libs_sumo.dart';
 
 import 'crypto/sodium_provider.dart';
